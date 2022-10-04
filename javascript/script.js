9ca1dd6de62225daa2546ac8c16ed85e53955ea6
unknown <a.moussawi.993@gmail.com>
2022-10-04 10:27:30 +0300

    create 'displayValue()' to display the values on the calculator screen

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,20 @@
+// displaying the values on calculator screen 
+function diplayValue(){
 let buttons = document.querySelectorAll("button");
 let equation='';
 let result;
 for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener("click", function () {
         equation += buttons[i].value;
+        if(buttons[i].value =="c"){
+            equation='';
+            result = document.getElementById('screen').innerHTML = equation;
+        }
         console.log(equation );
         result = document.getElementById('screen').innerHTML = equation;
     });
 }
 
+}
+
+diplayValue();
