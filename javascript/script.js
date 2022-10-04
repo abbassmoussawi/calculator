6f47b377e5d9b3a6b06db19e2a374fa7ab259009
unknown <a.moussawi.993@gmail.com>
2022-10-04 10:32:14 +0300

    Activate  the clear button

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,21 @@
 // displaying the values on calculator screen 
-function diplayValue(){
-let buttons = document.querySelectorAll("button");
-let equation='';
-let result;
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-        equation += buttons[i].value;
-        if(buttons[i].value =="c"){
-            equation='';
+function diplayValue() {
+    let buttons = document.querySelectorAll("button");
+    let equation = '';
+    let result;
+    //loop through all the class 'button'
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].addEventListener("click", function () {
+            equation += buttons[i].value;
+            //clear the screen
+            if (buttons[i].value == "c") {
+                equation = '';
+                result = document.getElementById('screen').innerHTML = equation;
+            }
+            //display the
+            console.log(equation);
             result = document.getElementById('screen').innerHTML = equation;
-        }
-        console.log(equation );
-        result = document.getElementById('screen').innerHTML = equation;
-    });
+        });
+    }
 }
-
-}
-
 diplayValue();
