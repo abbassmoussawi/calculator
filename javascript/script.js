09d9096983de78f9157b5cf50e49e836729bdf33
unknown <a.moussawi.993@gmail.com>
2022-10-05 17:15:22 +0300

    Write the logic and enhancement the code

--- javascript/script.js
+++ javascript/script.js
@@ -1,21 +1,41 @@
-// displaying the values on calculator screen 
+let buttons = document.querySelectorAll("button");
+let equation = '';
+let result;
+let term1;
+let term2;
+let opeartorPosition;
+
 function diplayValue() {
-    let buttons = document.querySelectorAll("button");
-    let equation = '';
-    let result;
     //loop through all the class 'button'
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener("click", function () {
-            equation += buttons[i].value;
+
             //clear the screen
             if (buttons[i].value == "c") {
                 equation = '';
-                result = document.getElementById('screen').innerHTML = equation;
+                document.getElementById('screen').innerHTML = equation;
+            } else if (buttons[i].value === '=') {
+                document.getElementById('screen').innerHTML = eval(equation);
+                equation =eval(equation);
+                console.log(equation, typeof equation);
+            } else if (buttons[i].value === '←') {
+              if (typeof equation =='string'){
+                equation = equation.slice(0, equation.length -1);
+                document.getElementById('screen').innerHTML = equation;
+              }else{
+                // document.getElementById('screen').innerHTML = 'asss';
+                console.log('Ther\'s nothing to remove, please press Clear button and make a new equation');
+              }
+            }else {
+                equation += buttons[i].value;
+                document.getElementById('screen').innerHTML = equation;
+
             }
-            //display the
-            console.log(equation);
-            result = document.getElementById('screen').innerHTML = equation;
-        });
-    }
+
+        }
+        )
+    };
 }
+
 diplayValue();
+
